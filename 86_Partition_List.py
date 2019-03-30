9590edc65519449281af744cc8100ae65bf99c0e
Yigal Irani <yigal.irani@beacon.io>
2019-03-30 00:32:31 -0400

    added alt implementation partition_without_vectors

--- 86_Partition_List.py
+++ 86_Partition_List.py
@@ -1,4 +1,3 @@
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -6,7 +5,8 @@
 #         self.next = None
 
 class Solution(object):
-    def partition(self, head, x):
+
+    def partition_using_vectors(self, head, x):
         """
         :type head: ListNode
         :type x: int
@@ -16,18 +16,48 @@ class Solution(object):
         bigger=[]
         while head:
             if head.val>=x:
-                bigger.append(x)
+                bigger.append(head.val)
             else:
-                smaller.append(x)
+                smaller.append(head.val)
+            head=head.next
         smaller.extend(bigger)
         last=None
-        for x in smaller.reverse():
-            head=Head(x)
-            head.next=last
-            last=head
+        smaller.reverse()
+        for x in smaller:
+            new_head=ListNode(x)
+            new_head.next=last
+            last=new_head
         return last
+    def partition_without_vectors(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        def set_next(head,the_next):
+            head.next=the_next
+            return head
+        def reverse(head,last):
+            while head:
+                next_head=head.next
+                head.next=last
+                last=head
+                head=next_head
+            return last
+        smaller=None
+        bigger=None
+        while head:
+            next_head=head.next
+            if head.val>=x:
+                bigger=set_next(head,bigger)
+            else:
+                smaller=set_next(head,smaller)
+            head=next_head
+            
+        return reverse(smaller,reverse(bigger,None))
+            
+    def partition(self, head, x):
+        return self.partition_without_vectors(head,x)
             
             
-            
-for x in list(range(10)).reverse():
-    print(x)
+        
