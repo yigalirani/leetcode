751191beacc47b41f439485b28b462dff70370d1
Yigal Irani <yigal@symbolclick.com>
2019-05-24 14:39:55 -0400

    done the iter version of the google question'

--- 20190501_google_phone_interview_question.py
+++ 20190501_google_phone_interview_question.py
@@ -25,15 +25,40 @@ def get_alloc_iter(n,k):
             return stack[top]
         
 '''        
-def get_alloc(n,k):
+def get_alloc2(n,k):
     if k==1:
         return [[n]]
     ans=[]
     for i in range(0,n+1):
-        for x in get_alloc(n-i,k-1):
+        for x in get_alloc2(n-i,k-1):
             ans.append(x+[i])
     return ans
 
-for i,x in enumerate(get_alloc(5,2)):
-    print(i,x,sum(x))
-    
+def get_alloc(n,k):
+    return [tuple(x) for x in get_alloc2(n,k)]
+
+def get_alloc_iter(n,k):
+    cur=[(n,k,[])]
+    while True:
+        nxt=[]
+        all_done=True
+        for n,k,r in cur:
+            if k==0:
+                nxt.append((n,k,r))
+                continue
+            all_done=False
+            #print ('>>>>>',n,k,r)
+            for i in range(n+1):
+                #print(i)
+                if k>1 or i==n:
+                    nxt.append((n-i,k-1,r+[i]))
+        if all_done:
+            return [tuple(x[2]) for x in nxt]
+        cur=nxt
+
+a1=set(get_alloc(40,5))
+a2=set(get_alloc_iter(20,5))
+#print(a1)
+print(a2)
+print(len(a1))
+print (a1.difference(a2))
